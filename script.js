56befa9d2fabf372c08bd161d4ae4c7116e44cbb
Florent Guiliana <florent.guiliana@gmail.com>
2024-11-13 23:11:02 +0100

    add blocks

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Bienvenue chez Fludo !");
 });
+
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+  span.addEventListener('click', (e) => {
+    e.target.classList.add('active');
+  });
+  span.addEventListener('animationend', (e) => {
+    e.target.classList.remove('active');
+  });
+  
+  // Initial animation
+  setTimeout(() => {
+    span.classList.add('active');
+  }, 750 * (idx+1))
+});
